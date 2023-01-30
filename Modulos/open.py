cb3b7e480ffd01b3c034fdfc8ffe48d079ff86ad
RecProjects <121030063+RecProjects@users.noreply.github.com>
2023-01-30 11:34:15 -0300

    cor

--- Modulos/open.py
+++ Modulos/open.py
@@ -226,7 +226,7 @@ def main(host=IP, port=PORT):
     print "\033[0;34m━"*8,"\033[1;32m PROXY SOCKS","\033[0;34m━"*8,"\n"
     print "\033[1;33mIP:\033[1;32m " + IP
     print "\033[1;33mPORTA:\033[1;32m " + str(PORT) + "\n"
-    print "\033[0;34m━"*10,"\033[1;32m SSHPLUS","\033[0;34m━\[\[\033[1;91m\]\]"*11,"\n"
+    print "\033[0;34m━"*10,"\033[1;32m SSHPLUS","\033[0;34m━\[\033[1;91m\]"*11,"\n"
     server = Server(IP, PORT)
     server.start()
     while True:
